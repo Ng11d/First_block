19f19c15ded1a2820ef83b89bbba818dd8d8ed02
Ng11d <ghoshnabamita05@gmail.com>
2023-08-16 13:39:06 +0530

    final_result

--- movement.py
+++ movement.py
@@ -1,95 +1,96 @@
+class Spacecraft:
+    def __init__(self, int_pos, int_dir):
+        self.position = int_pos
+        self.direction = int_dir
+
+    def move_forward(self):
+        if self.direction == 'N':
+            self.position[1] += 1
+        elif self.direction == 'S':
+            self.position[1] -= 1
+        elif self.direction == 'E':
+            self.position[0] += 1
+        elif self.direction == 'W':
+            self.position[0] -= 1
+        elif self.direction == 'Up':
+            self.position[2] += 1
+        elif self.direction == 'Down':
+            self.position[2] -= 1
+
+    def move_backward(self):
+        if self.direction == 'N':
+            self.position[1] -= 1
+        elif self.direction == 'S':
+            self.position[1] += 1
+        elif self.direction == 'E':
+            self.position[0] -= 1
+        elif self.direction == 'W':
+            self.position[0] += 1
+        elif self.direction == 'Up':
+            self.position[2] -= 1
+        elif self.direction == 'Down':
+            self.position[2] += 1
+
+    def turn_left(self):
+        if self.direction == 'N':
+            self.direction = 'W'
+        elif self.direction == 'S':
+            self.direction = 'E'
+        elif self.direction == 'E':
+            self.direction = 'N'
+        elif self.direction == 'W':
+            self.direction = 'S'
+
+    def turn_right(self):
+        if self.direction == 'N':
+            self.direction = 'E'
+        elif self.direction == 'S':
+            self.direction = 'W'
+        elif self.direction == 'E':
+            self.direction = 'S'
+        elif self.direction == 'W':
+            self.direction = 'N'
+
+    def turn_up(self):
+        if self.direction == 'N':
+            self.direction = 'Up'
+        elif self.direction == 'S':
+            self.direction = 'Down'
+        elif self.direction == 'E' or self.direction == 'W':
+            pass
+
+    def turn_down(self):
+        if self.direction == 'N':
+            self.direction = 'Down'
+        elif self.direction == 'S':
+            self.direction = 'Up'
+        elif self.direction == 'E' or self.direction == 'W':
+            pass
 
 
-initial_pos=[0,0,0]
-initial_direction='S'
-
-
-def move_forward(position,direction):
-    if direction == 'N':
-        position[1] += 1
-    elif direction == 'S':
-        position[1] -= 1
-    elif direction == 'E':
-        position[0] += 1
-    elif direction == 'W':
-        position[0] -= 1
-    elif direction == 'Up':
-        position[2] += 1
-    elif direction == 'Down':
-        position[2] -= 1
-
-
-def move_backward(position, direction):
-    if direction == 'N':
-        position[1] -= 1
-    elif direction == 'S':
-        position[1] += 1
-    elif direction == 'E':
-        position[0] -= 1
-    elif direction == 'W':
-        position[0] += 1
-    elif direction == 'Up':
-        position[2] -= 1
-    elif direction == 'Down':
-        position[2] += 1
-
-
-def turn_left(direction):
-    if direction == 'N':
-        direction = 'W'
-    elif direction == 'S':
-        direction = 'E'
-    elif direction == 'E':
-        direction = 'N'
-    elif direction == 'W':
-        direction = 'S'
-
-
-def turn_right(direction):
-    if direction == 'N':
-        direction = 'E'
-    elif direction == 'S':
-        direction = 'W'
-    elif direction == 'E':
-        direction = 'S'
-    elif direction == 'W':
-        direction = 'N'
-
-
-def turn_up(direction):
-    if direction == 'N':
-        direction = 'Up'
-    elif direction == 'S':
-        direction = 'Down'
-    elif direction == 'E' or direction == 'W':
-         pass
-
-
-def turn_down(direction):
-    if direction == 'N':
-        direction = 'Down'
-    elif direction == 'S':
-        direction = 'Up'
-    elif direction == 'E' or direction == 'W':
-        pass
-
-
-def execute_commands(array):
-    for command in array:
+def execute_commands(spacecraft, command_array):
+    for command in command_array:
         if command == 'f':
-            move_forward(initial_pos,initial_direction)
+            spacecraft.move_forward()
         elif command == 'b':
-            move_backward(initial_pos,initial_direction)
+            spacecraft.move_backward()
         elif command == 'l':
-            turn_left(initial_direction)
+            spacecraft.turn_left()
         elif command == 'r':
-            turn_right(initial_direction)
+            spacecraft.turn_right()
         elif command == 'u':
-            turn_up(initial_direction)
+            spacecraft.turn_up()
         elif command == 'd':
-            turn_down(initial_direction)
+            spacecraft.turn_down()
 
 
-execute_commands(['f', 'b', 'r', 'b', 'f'])
-print(initial_pos)
-print(initial_direction)
+# Example usage
+initial_position = [0, 0, 0]
+initial_direction = 'N'
+commands = ['f', 'r', 'u', 'b', 'l']
+
+cn = Spacecraft(initial_position, initial_direction)
+execute_commands(cn, commands)
+
+print("Final position:", cn.position)
+print("Final direction:", cn.direction)
--- position.py
+++ position.py
@@ -1,70 +1,13 @@
-def move_forward(position,direction):
-    if direction == 'N':
-        position[1] += 1
-    elif direction == 'S':
-        position[1] -= 1
-    elif direction == 'E':
-        position[0] += 1
-    elif direction == 'W':
-        position[0] -= 1
-    elif direction == 'Up':
-        position[2] += 1
-    elif direction == 'Down':
-        position[2] -= 1
+import unittest
+
+from movement import Spacecraft
 
 
-def move_backward(position, direction):
-    if direction == 'N':
-        position[1] -= 1
-    elif direction == 'S':
-        position[1] += 1
-    elif direction == 'E':
-        position[0] -= 1
-    elif direction == 'W':
-        position[0] += 1
-    elif direction == 'Up':
-        position[2] -= 1
-    elif direction == 'Down':
-        position[2] += 1
+class ValueTestCase(unittest.TestCase):
 
+    def valid_position(self):
 
-def turn_left(direction):
-    if direction == 'N':
-        direction = 'W'
-    elif direction == 'S':
-        direction = 'E'
-    elif direction == 'E':
-        direction = 'N'
-    elif direction == 'W':
-        direction = 'S'
-
-
-def turn_right(direction):
-    if direction == 'N':
-        direction = 'E'
-    elif direction == 'S':
-        direction = 'W'
-    elif direction == 'E':
-        direction = 'S'
-    elif direction == 'W':
-        direction = 'N'
-
-
-def turn_up(direction):
-    if direction == 'N':
-        direction = 'Up'
-    elif direction == 'S':
-        direction = 'Down'
-    elif direction == 'E' or direction == 'W':
-         pass
-
-
-def turn_down(direction):
-    if direction == 'N':
-        direction = 'Down'
-    elif direction == 'S':
-        direction = 'Up'
-    elif direction == 'E' or direction == 'W':
-        pass
-
-
+        ct=Spacecraft([0,0,0],'N')
+        result= Spacecraft.execute_commands(ct,['f', 'r', 'u', 'b', 'l'])
+        self.assertEqual(result.position,[0,-1,1])
+        self.assertEqual(result.direction, 'N')
