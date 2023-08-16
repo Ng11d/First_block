204dd79354168dca1985065954bccd17f7527c54
Ng11d <ghoshnabamita05@gmail.com>
2023-08-16 12:38:25 +0530

    space_craft_position

---
+++ movement.py
@@ -0,0 +1,2 @@
+from position import initial_position
+
---
+++ position.py
@@ -0,0 +1,10 @@
+def initial_position(x,y,z,dir):
+    print("Series of movement forward/backward (f, b),left/right (l, r),up/down (u, d)")
+    array=[]
+    for i in range(6):
+        array.append(input(array[i]))
+    
+
+
+
+
