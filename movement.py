041d87bd05a7c73d923e549f398635f4df52dbff
Ng11d <ghoshnabamita05@gmail.com>
2023-08-16 13:23:57 +0530

    one_func

--- movement.py
+++ movement.py
@@ -1,12 +1,79 @@
-from position import move_forward,move_backward,turn_left,turn_right,turn_up,turn_down
 
-    n=int(input("Print the number of inputs"))
-    print("Series of movement forward/backward (f, b),left/right (l, r),up/down (u, d)")
-    array=[]
-    for i in range(n):
-        array.append(input(array[i]))
-    initial_pos=[0,0,0]
-    initial_direction='N'
+
+initial_pos=[0,0,0]
+initial_direction='S'
+
+
+def move_forward(position,direction):
+    if direction == 'N':
+        position[1] += 1
+    elif direction == 'S':
+        position[1] -= 1
+    elif direction == 'E':
+        position[0] += 1
+    elif direction == 'W':
+        position[0] -= 1
+    elif direction == 'Up':
+        position[2] += 1
+    elif direction == 'Down':
+        position[2] -= 1
+
+
+def move_backward(position, direction):
+    if direction == 'N':
+        position[1] -= 1
+    elif direction == 'S':
+        position[1] += 1
+    elif direction == 'E':
+        position[0] -= 1
+    elif direction == 'W':
+        position[0] += 1
+    elif direction == 'Up':
+        position[2] -= 1
+    elif direction == 'Down':
+        position[2] += 1
+
+
+def turn_left(direction):
+    if direction == 'N':
+        direction = 'W'
+    elif direction == 'S':
+        direction = 'E'
+    elif direction == 'E':
+        direction = 'N'
+    elif direction == 'W':
+        direction = 'S'
+
+
+def turn_right(direction):
+    if direction == 'N':
+        direction = 'E'
+    elif direction == 'S':
+        direction = 'W'
+    elif direction == 'E':
+        direction = 'S'
+    elif direction == 'W':
+        direction = 'N'
+
+
+def turn_up(direction):
+    if direction == 'N':
+        direction = 'Up'
+    elif direction == 'S':
+        direction = 'Down'
+    elif direction == 'E' or direction == 'W':
+         pass
+
+
+def turn_down(direction):
+    if direction == 'N':
+        direction = 'Down'
+    elif direction == 'S':
+        direction = 'Up'
+    elif direction == 'E' or direction == 'W':
+        pass
+
+
 def execute_commands(array):
     for command in array:
         if command == 'f':
@@ -22,3 +89,7 @@ def execute_commands(array):
         elif command == 'd':
             turn_down(initial_direction)
 
+
+execute_commands(['f', 'b', 'r', 'b', 'f'])
+print(initial_pos)
+print(initial_direction)
